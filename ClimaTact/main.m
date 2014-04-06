56c70dabcec38dec56139521295c10715c7f977c
alswl <alswlx@gmail.com>
2014-04-06 19:08:31 +0800

    Initial Commit

---
+++ ClimaTact/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClimaTact
+//
+//  Created by Jason Ti on 4/6/14.
+//  Copyright (c) 2014 Jason Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CTAppDelegate class]));
+    }
+}
